c5b4bab6c8a8b7b987cd5389bc4f2f5320e8900d
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-11-07 11:17:48 +0500

    change models

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class UserModel(models.Model):
         (1, 'Paid'),
     )
 
+
     status = models.IntegerField(choices=STATUS)
     full_name = models.CharField(max_length=120)
     phone_number = models.CharField(max_length=13, unique=True)
