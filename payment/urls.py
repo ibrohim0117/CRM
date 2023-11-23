5699d456e30f26d95e017b9ad1d5a621c5cedd5f
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-11-23 20:04:43 +0500

    start payment app

--- payment/urls.py
+++ payment/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from payment.views import PaymentListCreateAPIView
+from payment.views import PaymentListCreateAPIView, PaymentRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('payment/', PaymentListCreateAPIView.as_view()),
+    path('payment/<int:pk>/', PaymentRetrieveUpdateDestroyAPIView.as_view())
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import permissions
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
+from order.permissions import IsOrderOwnerAuthenticatedOrReadOnly
 from payment.models import PaymentModel
 from payment.serializers import PaymentSerializers
 
@@ -8,3 +10,16 @@ from payment.serializers import PaymentSerializers
 class PaymentListCreateAPIView(ListCreateAPIView):
     queryset = PaymentModel.objects.all()
     serializer_class = PaymentSerializers
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get_queryset(self):
+        # Foydalanuvchi (customer) ID sini olish
+        customer_id = self.request.user.id
+        print(customer_id)
+        return PaymentModel.objects.filter(client__customer_id=customer_id)
+
+
+class PaymentRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = PaymentModel.objects.all()
+    serializer_class = PaymentSerializers
+    permission_classes = (IsOrderOwnerAuthenticatedOrReadOnly, )
